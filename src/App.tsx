7e0803280d15e929be2003e913dfb8ddd2f73e28
Jeevasurya Palanisamy <jeevasurya.datascientist@outlook.com>
2025-06-29 18:01:45 +0530

    route issue 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// src/App.tsx
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,13 +12,26 @@ import Loader from "./components/Loader";
 import ScrollToTop from "./components/ScrollToTop";
 import PageWrapper from "./components/PageWrapper"; // Import the wrapper
 
-// Page Imports
+// Main Page Imports
 import Index from "./pages/Index";
 import AdmissionForm from "./pages/AdmissionForm";
-// ... (other page imports)
+import CampusTour from "./pages/CampusTour";
+import PlacementDetails from "./pages/PlacementDetails";
+import HostelTransport from "./pages/HostelTransport";
+import ClubsCells from "./pages/ClubsCells";
+import Academics from "./pages/Academics";
 import NotFound from "./pages/NotFound";
+
+// Department Page Imports
 import ComputerEngineering from "./pages/departments/ComputerEngineering";
-// ... (other department page imports)
+import CivilEngineering from "./pages/departments/CivilEngineering";
+import MechanicalEngineering from "./pages/departments/MechanicalEngineering";
+import AutomobileEngineering from "./pages/departments/AutomobileEngineering";
+import DMLT from "./pages/departments/DMLT";
+import ChemicalEngineering from "./pages/departments/ChemicalEngineering";
+import PetrochemicalEngineering from "./pages/departments/PetrochemicalEngineering";
+import ElectricalAndElectronicsEngineering from "./pages/departments/ElectricalAndElectronicsEngineering";
+import ElectronicsAndCommunicationEngineering from "./pages/departments/ElectronicsAndCommunicationEngineering";
 
 const queryClient = new QueryClient();
 
@@ -32,9 +47,24 @@ const AppContent = () => {
           {/* Wrap every page's element with PageWrapper */}
           <Route path="/" element={<PageWrapper><Index /></PageWrapper>} />
           <Route path="/admission" element={<PageWrapper><AdmissionForm /></PageWrapper>} />
-          {/* ... WRAP ALL OTHER ROUTES SIMILARLY ... */}
+          <Route path="/campus-tour" element={<PageWrapper><CampusTour /></PageWrapper>} />
+          <Route path="/placement-details" element={<PageWrapper><PlacementDetails /></PageWrapper>} />
+          <Route path="/hostel-transport" element={<PageWrapper><HostelTransport /></PageWrapper>} />
+          <Route path="/clubs-cells" element={<PageWrapper><ClubsCells /></PageWrapper>} />
+          <Route path="/academics" element={<PageWrapper><Academics /></PageWrapper>} />
+
+          {/* Department Pages */}
           <Route path="/departments/computer-engineering" element={<PageWrapper><ComputerEngineering /></PageWrapper>} />
-          {/* ... WRAP ALL OTHER DEPARTMENT ROUTES ... */}
+          <Route path="/departments/chemical-engineering" element={<PageWrapper><ChemicalEngineering /></PageWrapper>} />
+          <Route path="/departments/petrochemical-engineering" element={<PageWrapper><PetrochemicalEngineering /></PageWrapper>} />
+          <Route path="/departments/eee" element={<PageWrapper><ElectricalAndElectronicsEngineering /></PageWrapper>} />
+          <Route path="/departments/ece" element={<PageWrapper><ElectronicsAndCommunicationEngineering /></PageWrapper>} />
+          <Route path="/departments/civil-engineering" element={<PageWrapper><CivilEngineering /></PageWrapper>} />
+          <Route path="/departments/mechanical-engineering" element={<PageWrapper><MechanicalEngineering /></PageWrapper>} />
+          <Route path="/departments/automobile-engineering" element={<PageWrapper><AutomobileEngineering /></PageWrapper>} />
+          <Route path="/departments/dmlt" element={<PageWrapper><DMLT /></PageWrapper>} />
+          
+          {/* Not Found Page */}
           <Route path="*" element={<PageWrapper><NotFound /></PageWrapper>} />
         </Routes>
       </BrowserRouter>
